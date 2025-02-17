1ad1d857446c1751339aaa317cb7953ed6d8cc71
bot_fx <oluwadamilareobalana@gmail.com>
2025-02-17 11:43:54 +0100

    Fixed Navigation Menu

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,4 @@ const hamburger = document.querySelector("#hamburger");
 hamburger.addEventListener("click", ()=>{
     navigation.classList.toggle("open");
 })
-const countries = ["Canada", "Chili", "Colombia", "Croatia", "Costa Rica"];
-const countriesLong = countries.filter((country) => country.length > 7);
-alert(countries.length - 1);
 
-const discount = 15;
-alert(discount <= 12 && discount >= 3);
