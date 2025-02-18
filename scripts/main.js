5f6023f072f35e91152ed476041c1cbe360362db
bot_fx <oluwadamilareobalana@gmail.com>
2025-02-18 10:06:54 +0100

    Home Page Update

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,55 @@
+// Hamburger Menu
 const navigation = document.querySelector("#navigation");
 const hamburger = document.querySelector("#hamburger");
 hamburger.addEventListener("click", ()=>{
     navigation.classList.toggle("open");
 })
 
+// Load data from projects json file
+const url = "./scripts/projects.json";
+const content = document.querySelector(".content");
+const modal = document.querySelector("dialog");
+async function loadData(url) {
+    const data = await fetch(url);
+    if(data.ok){
+        const result = await data.json();
+        console.log(result);
+        displayData(result);
+    }
+}
+function displayData(datum) {
+    datum.forEach(datum => {
+        const div = document.createElement("div");
+        const name = document.createElement("h3");
+        const img = document.createElement("img");
+        const description = document.createElement("p");
+        const button = document.createElement("button");
+        const close = document.createElement("button");
+        const link = document.createElement("a");
+
+        name.textContent = datum.name;
+        description.textContent = datum.description;
+        img.src = datum.image;
+        img.alt = `Image of ${datum.name}`;
+        img.loading = "lazy";
+        button.textContent = "Check Details";
+        close.textContent = "Close";
+        link.textContent = `Website to ${datum.name}`;
+        link.href = datum.link;
+        link.target = "_blank";
+
+        div.append(name, img, button);
+        content.appendChild(div);
+
+        button.addEventListener("click", ()=>{
+            modal.innerHTML = "";
+            const name1 = name.cloneNode(true);
+            modal.append(name1, description, link, close);
+            modal.showModal();
+            close.addEventListener("click", ()=>{
+                modal.close();
+            })
+        })
+    });
+}
+loadData(url);
